5c01c54849e5c6f0fe542a5c97ea87e268c2536f
abdul-korim-web <moneyschorsing@gmail.com>
2025-11-16 22:44:05 +0600

    error Handeler create

--- index.js
+++ index.js
@@ -12,9 +12,13 @@ dotenv.config()
 
 
 
-
-
-
+//  error handeler
+const errorHandeler= (err,req,res,next)=>{
+    console.log(err?.message || err);
+    res.status(400).json({status:400,message:"This is a server site error.Please Contact to Developer "})
+    next(err?.message || err)
+}
+app.use(errorHandeler)
 
 // server listen
 const port = process.env.PORT || 3000
