f4df093a165c5b19f39409d23cc2aa1465da3cd9
abdul-korim-web <moneyschorsing@gmail.com>
2025-11-17 21:58:08 +0600

    login api added

--- src/Controls/loginControl.js
+++ src/Controls/loginControl.js
@@ -1,12 +1,26 @@
 import { validationResult } from "express-validator";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { User } from "../Schemas/userSchema.js";
 
-export const loginControl = (req, res, next) => {
+export const loginControl = async(req, res, next) => {
   try {
+    const {email,password} = req.body
     const infoValidetionResult = validationResult(req);
-    console.log(infoValidetionResult);
     if (!infoValidetionResult.isEmpty()) {
       return res.status(400).json({ status: 400, message: infoValidetionResult?.errors[0].msg ||"unknown Error" });
     }
+    const isValidEmail =await User.findOne({email:email})
+    
+    if(!isValidEmail){
+        return res.status(400).json({status:400,message:"Email Is Not Exit "})
+    }
+    const isValidPassword =await bcrypt.compare(password,isValidEmail.password)
+    if(!isValidPassword){
+         return res.status(400).json({status:400,message:" password is incorrect "})
+    }
+    const token = await jwt.sign({id:isValidEmail?._id,email:isValidEmail?.email},process.env.JWT_SECRET,{expiresIn:`1d`})
+    res.status(200).json({status:200,message:"Login Success😍",token})
   } catch (error) {
     next(error)
   }
